2003c21ae39b92711e3460888342936e131c722c
JackNulty <jdnulty19@gmail.com>
2023-02-03 20:18:54 +0000

    added basic file structure and sfml and thor

---
+++ Temple-Root/Temple-Root/main.cpp
@@ -0,0 +1,47 @@
+#ifdef _DEBUG  
+#pragma comment(lib,"sfml-graphics-d.lib")  
+#pragma comment(lib,"sfml-audio-d.lib")  
+#pragma comment(lib,"sfml-system-d.lib")  
+#pragma comment(lib,"sfml-window-d.lib")  
+#pragma comment(lib,"sfml-network-d.lib")  
+#else  
+#pragma comment(lib,"sfml-graphics.lib")  
+#pragma comment(lib,"sfml-audio.lib")  
+#pragma comment(lib,"sfml-system.lib")  
+#pragma comment(lib,"sfml-window.lib")  
+#pragma comment(lib,"sfml-network.lib")  
+#endif  
+
+#include <iostream>
+#include <SFML/Graphics.hpp> 
+
+void main()
+{
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML Project");
+
+	sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Clock clock;
+	clock.restart();
+
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		timeSinceLastUpdate += clock.restart();
+
+		if (timeSinceLastUpdate > timePerFrame)
+		{
+			window.display();
+
+			timeSinceLastUpdate = sf::Time::Zero;
+		}
+	}
+}
